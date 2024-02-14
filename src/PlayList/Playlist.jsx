f3106334697dd1452275f764d043a5c2dd2c170f
LHufton <lukehufton@gmail.com>
2024-02-13 23:17:04 -0600

    file structure and playlist component

---
+++ src/PlayList/Playlist.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import './PlayList.css'
+import TrackList from '../TrackList/TrackList'
+
+const Playlist = ({
+  playlistName,
+  playlistTracks,
+  onNameChange,
+  onRemove,
+  onSave
+}) => {
+  const [name, setName] = useState(playlistName)
+
+  const handleNameChange = (event) => {
+    const newName = event.target.value
+    setName(newName)
+    onNameChange(newName)
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && name) {
+      onSave()
+    }
+  }
+
+  const handleClick = (event) => {
+    event.target.setSelectionRange(0, event.target.value.length)
+  }
+
+  return (
+    <div className="Playlist">
+      <input
+        id="Playlist-name"
+        placeholder="Enter a playlist name"
+        value={name}
+        onChange={handleNameChange}
+        onKeyPress={handleKeyPress}
+        onClick={handleClick}
+      />
+      <TrackList tracks={playlistTracks} onRemove={onRemove} isRemoval={true} />
+      <button className="Playlist-save" onClick={onSave}>
+        SAVE TO SPOTIFY
+      </button>
+    </div>
+  )
+}
+
+export default Playlist
