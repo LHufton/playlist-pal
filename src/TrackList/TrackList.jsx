bbb7efc4ff9ac801e49bef6c01748b1a32c5daf6
LHufton <lukehufton@gmail.com>
2024-02-13 23:38:41 -0600

    added trackList component

---
+++ src/TrackList/TrackList.jsx
@@ -0,0 +1,35 @@
+import './TrackList.css'
+import Track from '../Track/Track'
+import PropTypes from 'prop-types'
+
+const TrackList = ({ tracks, onAdd, onRemove, isRemoval }) => {
+  return (
+    <div className="TrackList">
+      {tracks.map((track) => (
+        <Track
+          key={track.id}
+          track={track}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          isRemoval={isRemoval}
+        />
+      ))}
+    </div>
+  )
+}
+
+TrackList.propTypes = {
+  tracks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string,
+      artist: PropTypes.string,
+      album: PropTypes.string
+    })
+  ).isRequired,
+  onAdd: PropTypes.func,
+  onRemove: PropTypes.func,
+  isRemoval: PropTypes.bool
+}
+
+export default TrackList
