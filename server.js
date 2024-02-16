c67557417912d0b7c35de05558b9955af2013263
LHufton <lukehufton@gmail.com>
2024-02-15 21:37:57 -0600

    file restructuring

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 const app = express()
 const PORT = process.env.PORT || 3001
+
+// Convert the URL of the current module to a file path
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name of the current module
+const __dirname = path.dirname(__filename)
 
 app.use((req, res, next) => {
   const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress
   req.clientIP = clientIP
   next()
 })
+
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
